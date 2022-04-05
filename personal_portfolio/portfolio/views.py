9aed0385cb31034591eca0f4c2b2f12aa840e450
jumpingtheshark <mike.rubinstein@gmail.com>
2022-04-05 13:39:31 -0500

    blah

--- personal_portfolio/portfolio/views.py
+++ personal_portfolio/portfolio/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse  # added
+from .models import Project
 
 
 # Create your views here.
 def home(request):  # function added
     return HttpResponse("home page")
+
+
+
+def home2 (request):
+    projects=Project.objects.all()
+    return render (request, 'folio/home.html', {'projects':projects})
