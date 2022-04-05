8268c3db4bae79ea8a0c26993db1833a6904a759
jumpingtheshark <mike.rubinstein@gmail.com>
2022-04-05 13:06:54 -0500

    hello world

--- personal_portfolio/portfolio/views.py
+++ personal_portfolio/portfolio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse  # added
+
 
 # Create your views here.
+def home(request):  # function added
+    return HttpResponse("home page")
