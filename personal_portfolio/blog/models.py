45dcc389cfd30544fbbd9c6e79323a82fdbc7472
michael rubinstein <mike.rubinstein@gmail.com>
2022-04-06 13:19:56 -0500

    5.43, 5.44

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class BlogPost(models.Model):
+    post_date= models.DateField()
+    title= models.TextField()
+    post_text = models.CharField(max_length=1000)
+    
+
+    def __str__(self):
+        return self.title
--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse  # added
+from .models import BlogPost #added 
 
 
 # Create your views here.
 
 
 def home(request):  # function added
-    return HttpResponse("home page")
+    return HttpResponse("blogs home page")
+
+def all_blogs (request): #function added
+    posts = BlogPost.objects.order_by('-post_date')[:5]
+    print (type(posts))
+    return render (request, 'blog/all_blogs.html', {'posts':posts})
+     
