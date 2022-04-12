8dc22458a07039adb926af1e91136fa2c7731eb1
Michael Rubinstein <mike.rubinstein@gmail.com>
2022-04-12 00:38:53 -0500

    5.46- detail page

--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse  # added
 from .models import BlogPost #added 
 
@@ -13,4 +13,9 @@ def all_blogs (request): #function added
     posts = BlogPost.objects.order_by('-post_date')[:5]
     print (type(posts))
     return render (request, 'blog/all_blogs.html', {'posts':posts})
+
+def detail (request, blog_id):
+    blog=get_object_or_404(BlogPost, pk=blog_id)
+    return render (request, 'blog/detail.html', {'blog':blog})
+
      
